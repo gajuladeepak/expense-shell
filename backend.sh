744e40d9fb29b202bb2974ab30a8b7d294584a7a
deepakgajula <gajuladeepak17@gmail.com>
2025-02-09 15:54:33 +0530

    expense-shell

--- backend.sh
+++ backend.sh
@@ -1,73 +1,84 @@
-#!/bin/bash
+#!bin/bash
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
-USERID=$(id -u)
 LOGS_FOLDER="/var/log/expense"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+LOGFILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 mkdir -p $LOGS_FOLDER
 
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
 
 CHECK_ROOT(){
     if [ $USERID -ne 0 ]
     then
-        echo -e "$R Please provide root privelegs $N" | tee -a $LOG_FILE
+        echo "$R Please run this script with root priveleges $N" | tee -a $LOGFILE
         exit 1
     fi
 }
 
-VALIDATE () {
+VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 is.... $R FAILED $N" | tee -a $LOG_FILE
+        echo -e "$2 is...$R FAILED $N" | tee -a $LOGFILE
         exit 1
     else
-        echo -e "$2 is.... $G SUCCESS $N" | tee -a $LOG_FILE
-
+        echo -e "$2 is... $G SUCCESS $N" | tee -a $LOGFILE
     fi
 }
 
+echo "Script started executing at: $(date)" | tee -a $LOGFILE
 CHECK_ROOT
 
-echo "Script started executing at: $(date)"
+dnf module disable nodejs -y &>>$LOGFILE
+VALIDATE $? "Disable default nodejs"
 
-
-
-
-dnf module disable nodejs -y &>>$LOG_FILE
-VALIDATE $? "Disabled nodejs"
-
-dnf module enable nodejs:20 -y &>>$LOG_FILE
+dnf module enable nodejs:20 -y &>>$LOGFILE
 VALIDATE $? "Enable nodejs:20"
 
-dnf install nodejs -y &>>$LOG_FILE
-VALIDATE $? "Installing nodejs"
+dnf install nodejs -y &>>$LOGFILE
+VALIDATE $? "Install nodejs"
 
-id expense &>>$LOG_FILE
+id expense &>>$LOGFILE
 if [ $? -ne 0 ]
 then
-    echo -e "Expense user id not created..... $G CREATE THE USER $N" 
-    useradd expense
-    VALIDATE $? "Creating User"
+    echo -e "expense user not exists... $G Creating User $N"
+    useradd expense &>>$LOGFILE
+    VALIDATE $? "Creating Expense User"
 else
-    echo -e "$Y Already User Has Been Created $N" 
+    echo -e "Expence user already exists... $Y SKIPPING $N"
 fi
 
-mkdir /app
-VALIDATE $? "Creating/app folder"
+mkdir -p /app
+VALIDATE $? "Creating /app folder"
 
-curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE
-VALIDATE $? "Downloading the code"
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOGFILE
+VALIDATE $? "Downloading bacckend application code"
 
 cd /app
-rm -rf /app/*
-unzip /tmp/backend.zip &>>$LOG_FILE
-VALIDATE $? "Extracting the code"
+rm -rf /app/* #remove the existing code
+unzip /tmp/backend.zip &>>$LOGFILE
+VALIDATE $? "Extracting backend application code"
 
-npm install &>>$LOG_FILE
-VALIDATE $? "Installing the dependencies"
+npm install &>>$LOGFILE
+cp /home/ec2-user/expence-shell/backend.service  /etc/systemd/system/backend.service
+
+# load the data before running backend
+
+dnf install mysql -y &>>$LOGFILE
+VALIDATE $? "Installing MYSQL Client"
+
+mysql -h mysql.deepakaws.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOGFILE
+VALIDATE $? "Schema Loading"
+
+systemctl daemon-reload &>>$LOGFILE
+VALIDATE $? "Demon reload"
+
+systemctl enable backend &>>$LOGFILE
+VALIDATE $? "Enabled backend"
+
+systemctl restart backend &>>$LOGFILE
+VALIDATE $? "Restarted Backend"
