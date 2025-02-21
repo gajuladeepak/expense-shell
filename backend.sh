947456ee70b1417e637e741c9b1feb01c9cb7c76
deepakgajula <gajuladeepak17@gmail.com>
2025-02-21 14:58:24 +0530

    backend_practice

--- backend.sh
+++ backend.sh
@@ -57,3 +57,32 @@ else
     echo -e " $Y User is already Created $N"
 fi
 
+mkdir -p /app
+VALIDATE $? "Creating DIRECTORY"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
+VALIDATE $? "Downloading the code"
+
+cd /app
+rm -rf /app/*
+unzip /tmp/backend.zip
+VALIDATE $? "Extracting backend application code"
+
+npm install &>>$LOG_FILE
+cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service
+
+
+dnf install mysql -y | &>>$LOG_FILE
+VALIDATE $? "Installing MYSQL CLIENT"
+
+mysql -h mysql.deepakaws.online -uroot -pExpenseApp@1 < /app/schema/backend.sql | &>>$LOG_FILE
+VALIDATE $? "Schema loading"
+
+systemctl daemon-reload &>>$LOG_FILE
+VALIDATE $? "Daemon reload"
+
+systemctl enable backend &>>$LOG_FILE
+VALIDATE $? "Enabled backend"
+
+systemctl restart backend &>>$LOG_FILE
+VALIDATE $? "Restarted Backend"
